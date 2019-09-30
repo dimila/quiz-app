bc22be8b9ec4c029bf0a8354cd4004fb503e022e
dimila <dimamilayev@Dimas-MacBook-Pro.local>
2019-09-30 11:52:00 +0200

    Setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components/macro'
 
 function App() {
-  return <div className="App">"Hello World"</div>
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `
+
+  //  return <div className="App">"Hello World"</div>
+  return (
+    <Wrapper>
+      <Title>"Hello World"</Title>
+    </Wrapper>
+  )
 }
 
 export default App
