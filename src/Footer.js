8f06b7df8881a7893f663732ee2c95cd1af3f46c
dimila <dimamilayev@Dimas-MacBook-Pro.local>
2019-10-16 10:31:48 +0200

    add whattsapp update

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,15 @@ import styled from 'styled-components'
 export default () => (
   <Footer>
     <FooterContainer>
+      <FooterSubscribe>
+        <FooterHead>
+          Subscribe for WhatsApp <br /> Updates
+        </FooterHead>
+        <form>
+          <FooterInput type="number" placeholder="Your phone number" />
+          <FooterButton>subscribe</FooterButton>
+        </form>
+      </FooterSubscribe>
       <FooterInfo>
         <FooterInfoDescription>
           <FooterInfoName>Dimitri Milayev</FooterInfoName>
@@ -15,6 +24,45 @@ export default () => (
     </FooterContainer>
   </Footer>
 )
+
+const FooterInput = styled.input`
+  font-size: 13px;
+  font-weight: 700;
+  background-color: #8abdcf;
+  color: #fff;
+  border: none;
+  border: 3px solid #ff4040;
+  border-radius: 10px;
+  padding: 13px 20px 13px 13px;
+`
+
+const FooterButton = styled.button`
+  font-size: 13px;
+  padding: 15px 30px;
+  border: none;
+  color: #fff;
+  margin-left: 10px;
+  background-color: #ff5052;
+  border-radius: 10px;
+  @media (max-width: 640px) {
+    margin-top: 10px;
+  }
+`
+
+const FooterSubscribe = styled.div`
+  display: flex;
+  flex: 1 1 0;
+  flex-direction: column;
+`
+
+const FooterHead = styled.h3`
+  font-size: 28px;
+  color: #2a2f36;
+  margin-bottom: 30px;
+  @media (max-width: 640px) {
+    font-size: 24px;
+  }
+`
 
 const Footer = styled.footer`
   padding-top: 65px;
